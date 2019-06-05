d3601911aaa114961d7914ae9ada628002e6bd79
rjp5219 <51171598+rjp5219@users.noreply.github.com>
2019-06-04 21:43:13 -0400

    Update ReverseString.java
    Reverse's a string

--- ReverseString.java
+++ ReverseString.java
@@ -13,6 +13,7 @@ public static String reverse(String str) {
     {
         String input;
             
+        //Used Dialog boxes to refresh memory 
         input = JOptionPane.showInputDialog("Enter a string:");
         JOptionPane.showMessageDialog(null, input + " reversed is " + reverse(input));
         }
