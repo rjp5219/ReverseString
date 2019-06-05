721f1180e3fad189309197083e7e3118afb29d80
rjp5219 <51171598+rjp5219@users.noreply.github.com>
2019-06-04 21:31:01 -0400

    Initial commit

---
+++ ReverseString.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import javax.swing.*;
+
+public class ReverseString {
+    
+public static String reverse(String str) {
+    if ((null == str) || (str.length() <= 1)) {
+        return str;
+    }
+    return reverse(str.substring(1)) + str.charAt(0);
+}
+    public static void main(String[] args)throws IOException
+    {
+        String input;
+            
+        input = JOptionPane.showInputDialog("Enter a string:");
+        JOptionPane.showMessageDialog(null, input + " reversed is " + reverse(input));
+        }
+    
+}
